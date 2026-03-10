ae87d605e728466f634827713af58544fd621bb9
Patricia Segantine <pnsegantine@gmail.com>
2026-03-10 20:58:28 +0000

    feat(server): add tests for POST /links route with validation scenarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { createLinkRoute } from "@/routes/create-link";
+import { createLinkRoute } from "@/routes/create-link/create-link.route";
 
 export async function routes(app: FastifyInstance) {
   await app.register(createLinkRoute);
