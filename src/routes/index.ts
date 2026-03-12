6c550f41edeeac4e6d228d709031faf0b63706f5
Patricia Segantine <pnsegantine@gmail.com>
2026-03-12 16:50:18 +0000

    Revert "feat(server): add GET /links route with pagination, sorting, and filtering"
    This reverts commit 7ea77b5bef235f306813fbf1797a5655540a0492.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { createLinkRoute } from "@/routes/create-link/create-link.route";
-import { getLinksRoute } from '@/routes/get-links';
 
 export async function routes(app: FastifyInstance) {
   await app.register(createLinkRoute);
-  await app.register(getLinksRoute);
 }
